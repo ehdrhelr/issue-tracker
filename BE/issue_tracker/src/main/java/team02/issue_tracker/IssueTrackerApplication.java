974c4151615fe8a00ca975e6e0555c9c8d9f51c4
Shion <ehdrhelr@gmail.com>
2021-07-12 21:01:11 +0900

    jasypt 적용

--- BE/issue_tracker/src/main/java/team02/issue_tracker/IssueTrackerApplication.java
+++ BE/issue_tracker/src/main/java/team02/issue_tracker/IssueTrackerApplication.java
@@ -1,8 +1,10 @@
 package team02.issue_tracker;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class IssueTrackerApplication {
 
